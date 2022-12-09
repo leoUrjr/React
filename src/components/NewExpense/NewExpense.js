c0db42fedbe2c09b45f6d8f692e7741cd53c485b
leoUrJr <109521306+leoUrjr@users.noreply.github.com>
2022-12-09 15:08:38 +0545

    Completed the 4th section

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+const NewExpense = (props) =>{
+    const onSaveExpenseDatahandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+       props.onAddExpense(expenseData); 
+    }; 
+
+    return <div className='new-expense'>
+       <ExpenseForm onSaveExpenseData={onSaveExpenseDatahandler} />
+    </div>
+};
+export default NewExpense;
